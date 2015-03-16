f82c5155dca5c9e1d453bdc95de7261ec0ca729d
Aldric Manzano <mageknightfave@yahoo.com.ph>
2015-03-16 17:52:11 +0800

    Update AbstractMicrobotColumn.py

--- src/abstract/represent/AbstractMicrobotColumn.py
+++ src/abstract/represent/AbstractMicrobotColumn.py
@@ -1,39 +1,15 @@
-class AbstractMicrobotColumn:
+import abstract.represent.LineSegment
+
+class AbstractMicrobotColumn(LineSegment):
 
     """This is the mathematical representation of the frame the Microbots will form when they take a shape.
         Microbots move from p0 to p1.
     """
 
     def __init__(self):
-        self.p0 = new Point()
-        self.p1 = new Point()
-        self.pu = new Point()
-        self.length = 0 # fine. we'll use "length."
+        LineSegment.__init__(self)
         self.microbotsInLine = []
 
-    def reevaluate(self):
-        self.pu.setPoint(self.p0.getUnitVectorTo(self.p1))
-        self.length = self.p0.getDist(self.p1)
-
-    def setP0(self, x, y, z):
-        self.p0.set(x, y, z)
-        reevaluate()
-        return self
-
-    def setP1(self, x, y, z):
-        self.p1.set(x, y, z)
-        reevaluate()
-        return self
-
-    def getClosestPoint(self, p):
-        """Gets the closest point from p on the line segment.
-        """
-
-        # get point to the line (extending infinitely.)
-        # get distance of that point to the endpoints.
-        # if a distance is greater than the line segment's length, then it's outside the line segment.
-        #    so get the endpoint which is closer to it.
-        # if not, then return that point.
 
     def pull(self, microbotArray):
 
